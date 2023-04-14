1a37c4679c398edfacad9a6e521ff7fe23ed61e1
Devin Lokugamage <35444889+devinlokugamage@users.noreply.github.com>
2023-04-14 08:53:51 -0400

    Update UserService.js

--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const USER_API_BASE_URL = "http://localhost:8080/api/v1/users";
+const USER_API_BASE_URL = "http://localhost:3000/api/v1/users";
 
 class UserService {
 
